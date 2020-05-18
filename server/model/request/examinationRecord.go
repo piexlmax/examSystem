d01b70c6ccc005ad771fad949fc97f872e031cc1
pixel <303176530@qq.com>
2020-05-18 17:56:02 +0800

    第二天

--- server/model/request/examinationRecord.go
+++ server/model/request/examinationRecord.go
@@ -1,7 +1,1 @@
-// 自动生成模板ExaminationRecord
-package request
-
-type ReqExaminationRecord struct {
-	SysUserID   string `json:"sysUserId" form:"sysUserId"`
-	TestPaperID string `json:"testPaperId" form:"testPaperId"`
-}
+package response
