1445efa0282682a15604f3f93f65a9c1f9cb6e5b
pixel <303176530@qq.com>
2020-05-20 09:37:23 +0800

    遮罩

--- web/src/view/examiner/testPaper/testPaperInfo.vue
+++ web/src/view/examiner/testPaper/testPaperInfo.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="svgInfo" v-loading.fullscreen.lock="loading">
-    <input type="file" v-if="!loading" v-show="false" ref="fileupload" @change="handlerUpload($event)">
+    <input
+      @change="handlerUpload($event)"
+      ref="fileupload"
+      type="file"
+      v-if="!loading"
+      v-show="false"
+    />
     <div v-html="svg"></div>
     <!--自定义右键菜单html代码-->
     <ul :style="{left:left+'px',top:top+'px'}" class="contextmenu" v-show="contextMenuVisible">
-      <li @click="upload('flow')" v-if="!activeNode.flow">上传流量文件</li> 
+      <li @click="upload('flow')" v-if="!activeNode.flow">上传流量文件</li>
       <li @click="download('flow')" v-if="activeNode.flow">下载流量文件</li>
       <li @click="clear('flow')" v-if="activeNode.flow">清除流量文件</li>
       <li @click="upload('configuration')" v-if="!activeNode.configuration">上传配置文件</li>
@@ -20,149 +26,169 @@
   </div>
 </template>
 <script>
-import { getTestPaperSvg ,findAndCreateTestPaperSvgNode,downloadTestPaperSvgNode,clearTestPaperSvgNode } from "@/api/testPaper";
-import axios from 'axios'; // 引入axios
+import {
+  getTestPaperSvg,
+  findAndCreateTestPaperSvgNode,
+  downloadTestPaperSvgNode,
+  clearTestPaperSvgNode
+} from '@/api/testPaper'
+import axios from 'axios' // 引入axios
 const service = axios.create({
-    baseURL: process.env.VUE_APP_BASE_API,
-    timeout: 99999
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 99999
 })
 export default {
-  name: "TestPaperInfo",
+  name: 'TestPaperInfo',
   data() {
     return {
-      svg: "",
+      svg: '',
       contextMenuVisible: false,
       left: 0,
       top: 0,
-      loading:false,
-      active:{},
-      activeNode:{},
-      formData:null,
-       isCollapse: false,
-      isMobile:false,
-    };
+      loading: false,
+      active: {},
+      activeNode: {},
+      formData: null,
+      isCollapse: false,
+      isMobile: false
+    }
   },
   methods: {
-      upload(type){
-        this.param = new FormData();
-        this.param.append("type", type);
-        this.$refs.fileupload.click()
-      },
-      async handlerUpload(e){
-        this.loading = true
-        this.param.append("file", e.target.files[0]);
-        this.param.append("nodeId",this.active.target.id)
-        this.param.append("testPaperID",Number(this.$route.query.id))
-        const token = this.$store.getters['user/token']
-        const res =  await service.post("/testPaper/uploadTestPaperSvgNode",this.param,{headers:{
+    upload(type) {
+      this.param = new FormData()
+      this.param.append('type', type)
+      this.$refs.fileupload.click()
+    },
+    async handlerUpload(e) {
+      this.loading = true
+      this.param.append('file', e.target.files[0])
+      this.param.append('nodeId', this.active.target.id)
+      this.param.append('testPaperID', Number(this.$route.query.id))
+      const token = this.$store.getters['user/token']
+      const res = await service.post(
+        '/testPaper/uploadTestPaperSvgNode',
+        this.param,
+        {
+          headers: {
             'x-token': token
-        }})
-        this.loading = false
-        if(res.data.code==0){
-          this.$message({
-            type:"success",
-            message:"上传成功"
-          })
-        }else{
-          this.$message({
-            type:"error",
-            message:res.data.msg
-          })
+          }
         }
-      },
-      async download(type){
-        const res = await downloadTestPaperSvgNode({path:this.activeNode[type]})
-        const blob = new Blob([res]);
-        const fileName = this.activeNode[type].split("/"+type+"/")[1];
-      if ("download" in document.createElement("a")) {
+      )
+      this.loading = false
+      if (res.data.code == 0) {
+        this.$message({
+          type: 'success',
+          message: '上传成功'
+        })
+      } else {
+        this.$message({
+          type: 'error',
+          message: res.data.msg
+        })
+      }
+    },
+    async download(type) {
+      const res = await downloadTestPaperSvgNode({
+        path: this.activeNode[type]
+      })
+      const blob = new Blob([res])
+      const fileName = this.activeNode[type].split('/' + type + '/')[1]
+      if ('download' in document.createElement('a')) {
         // 不是IE浏览器
-        let url = window.URL.createObjectURL(blob);
-        let link = document.createElement("a");
-        link.style.display = "none";
-        link.href = url;
-        link.setAttribute("download", fileName);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link); // 下载完成移除元素
-        window.URL.revokeObjectURL(url); // 释放掉blob对象
+        let url = window.URL.createObjectURL(blob)
+        let link = document.createElement('a')
+        link.style.display = 'none'
+        link.href = url
+        link.setAttribute('download', fileName)
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link) // 下载完成移除元素
+        window.URL.revokeObjectURL(url) // 释放掉blob对象
       } else {
         // IE 10+
-        window.navigator.msSaveBlob(blob, fileName);
+        window.navigator.msSaveBlob(blob, fileName)
       }
-      },
-      async clear(type){
-         const res = await clearTestPaperSvgNode({nodeId:this.active.target.id,testPaperID:Number(this.$route.query.id),type:type})
-         if(res.code == 0 ){
-           this.$message({
-             type:"success",
-             message:"清除成功"
-           })
-         }
-      },
+    },
+    async clear(type) {
+      const res = await clearTestPaperSvgNode({
+        nodeId: this.active.target.id,
+        testPaperID: Number(this.$route.query.id),
+        type: type
+      })
+      if (res.code == 0) {
+        this.$message({
+          type: 'success',
+          message: '清除成功'
+        })
+      }
+    },
     async openContextMenu(e) {
-      window.event.returnValue = false;
-      var oEvent = e || event;
+      window.event.returnValue = false
+      var oEvent = e || event
       this.active = e
-      const res = await findAndCreateTestPaperSvgNode({nodeId:this.active.target.id,testPaperID:Number(this.$route.query.id)})
-      if (res.code == 0){
-      this.activeNode = res.data.node
-      this.contextMenuVisible = true;
-      let width;
-      if (this.isCollapse) {
-        width = 54;
-      } else {
-        width = 220;
-      }
-      if (this.isMobile) {
-        width = 0;
-      }
-      this.left = oEvent.clientX - width;
-      this.top = oEvent.clientY + 10;
+      const res = await findAndCreateTestPaperSvgNode({
+        nodeId: this.active.target.id,
+        testPaperID: Number(this.$route.query.id)
+      })
+      if (res.code == 0) {
+        this.activeNode = res.data.node
+        this.contextMenuVisible = true
+        let width
+        if (this.isCollapse) {
+          width = 54
+        } else {
+          width = 220
+        }
+        if (this.isMobile) {
+          width = 0
+        }
+        this.left = oEvent.clientX - width
+        this.top = oEvent.clientY + 10
       }
     }
   },
   async created() {
-    this.$bus.on('mobile',(isMobile)=>{
+    this.$bus.on('mobile', isMobile => {
       this.isMobile = isMobile
     })
-    this.$bus.on('collapse',(isCollapse)=>{
+    this.$bus.on('collapse', isCollapse => {
       this.isCollapse = isCollapse
     })
-    const res = await getTestPaperSvg({ ID: Number(this.$route.query.id) });
+    const res = await getTestPaperSvg({ ID: Number(this.$route.query.id) })
     if (res.code == 0) {
-      this.svg = res.data.retestPaper;
+      this.svg = res.data.retestPaper
       setTimeout(() => {
-        const tspans = document.getElementsByTagName("tspan");
+        const tspans = document.getElementsByTagName('tspan')
         for (let k = 0; k < tspans.length; k++) {
-          const tspan = tspans[k].getElementsByTagName("tspan");
+          const tspan = tspans[k].getElementsByTagName('tspan')
           if (tspan[0]) {
-            tspan[0].oncontextmenu = this.openContextMenu;
+            tspan[0].oncontextmenu = this.openContextMenu
           }
         }
-      }, 0);
+      }, 0)
     }
   },
   watch: {
     contextMenuVisible() {
       if (this.contextMenuVisible) {
-        document.body.addEventListener("click", () => {
-          this.contextMenuVisible = false;
-        });
+        document.body.addEventListener('click', () => {
+          this.contextMenuVisible = false
+        })
       } else {
-        document.body.removeEventListener("click", () => {
-          this.contextMenuVisible = false;
-        });
+        document.body.removeEventListener('click', () => {
+          this.contextMenuVisible = false
+        })
       }
     }
   }
-};
+}
 </script>
 <style lang="scss">
 .svgInfo {
   display: flex;
   justify-content: center;
-  height:calc(100vh - 195px);
-  overflow:auto;
+  height: calc(100vh - 195px);
+  overflow: auto;
 }
 
 .contextmenu {
@@ -188,8 +214,8 @@ export default {
   cursor: pointer;
 }
 
-tspan{
-  tspan{
+tspan {
+  tspan {
     cursor: pointer;
   }
 }
